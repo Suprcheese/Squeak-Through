62c0cf745c4399c14d59cf028e6b7e5a51891305
Suprcheese <Suprcheese@users.noreply.github.com>
2016-04-26 21:57:32 -0700

    Version 1.0.0

---
+++ Readme.txt
@@ -0,0 +1,12 @@
+Squeak Through 1.0.0
+====================
+
+Version 1.0.0 was released September 20, 2015, was tested using Factorio v0.12.7, and was authored by Supercheese.
+
+This small mod reduces the collision boxes for many structures, allowing you to "squeak through" them while walking around your base.
+Say goodbye to the frustration of having your path blocked by your steam engines or solar panels when walking around your base!
+
+For a full list of structures altered, see the changelog.
+
+
+See also the associated forum thread: http://www.factorioforums.com/forum/viewtopic.php?f=91&t=16476
---
+++ data.lua
@@ -0,0 +1,52 @@
+data.raw["solar-panel"]["solar-panel"].collision_box = {{-1.25, -1.25}, {1.25, 1.25}}
+
+data.raw["accumulator"]["basic-accumulator"].collision_box = {{-0.75, -0.75}, {0.75, 0.75}}
+
+data.raw["generator"]["steam-engine"].collision_box = {{-1.25, -2.25}, {1.25, 2.25}}
+
+data.raw["pipe"]["pipe"].collision_box = {{-0.08, -0.08}, {0.08, 0.08}}
+
+data.raw["pipe-to-ground"]["pipe-to-ground"].collision_box = {{-0.08, -0.08}, {0.08, 0.08}}
+
+data.raw["container"]["wooden-chest"].collision_box = {{-0.25, -0.25}, {0.25, 0.25}}
+
+data.raw["container"]["iron-chest"].collision_box = {{-0.25, -0.25}, {0.25, 0.25}}
+
+data.raw["container"]["steel-chest"].collision_box = {{-0.25, -0.25}, {0.25, 0.25}}
+
+data.raw["smart-container"]["smart-chest"].collision_box = {{-0.25, -0.25}, {0.25, 0.25}}
+
+data.raw["logistic-container"]["logistic-chest-active-provider"].collision_box = {{-0.25, -0.25}, {0.25, 0.25}}
+
+data.raw["logistic-container"]["logistic-chest-passive-provider"].collision_box = {{-0.25, -0.25}, {0.25, 0.25}}
+
+data.raw["logistic-container"]["logistic-chest-requester"].collision_box = {{-0.25, -0.25}, {0.25, 0.25}}
+
+data.raw["logistic-container"]["logistic-chest-storage"].collision_box = {{-0.25, -0.25}, {0.25, 0.25}}
+
+data.raw["assembling-machine"]["chemical-plant"].collision_box = {{-1.25, -1.25}, {1.25, 1.25}}
+
+data.raw["assembling-machine"]["oil-refinery"].collision_box = {{-2.25, -2.25}, {2.25, 2.25}}
+
+data.raw["arithmetic-combinator"]["arithmetic-combinator"].collision_box = {{-0.25, -0.75}, {0.25, 0.75}}
+
+data.raw["decider-combinator"]["decider-combinator"].collision_box = {{-0.25, -0.75}, {0.25, 0.75}}
+
+data.raw["constant-combinator"]["constant-combinator"].collision_box = {{-0.25, -0.25}, {0.25, 0.25}}
+
+data.raw["boiler"]["boiler"].collision_box = {{-0.08, -0.08}, {0.08, 0.08}}
+
+data.raw["electric-pole"]["substation"].collision_box = {{-0.75, -0.75}, {0.75, 0.75}}
+
+data.raw["mining-drill"]["burner-mining-drill"].collision_box = {{-0.75, -0.75}, {0.75, 0.75}}
+
+data.raw["mining-drill"]["basic-mining-drill"].collision_box = {{-1.25, -1.25}, {1.25, 1.25}}
+
+data.raw["mining-drill"]["pumpjack"].collision_box = {{-1.25, -1.25}, {1.25, 1.25}}
+
+data.raw["pump"]["small-pump"].collision_box = {{-0.08, -0.08}, {0.08, 0.08}}
+
+data.raw["radar"]["radar"].collision_box = {{-1.25, -1.25}, {1.25, 1.25}}
+
+data.raw["storage-tank"]["storage-tank"].collision_box = {{-1.25, -1.25}, {1.25, 1.25}}
+
