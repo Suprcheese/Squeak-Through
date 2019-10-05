5b58d100c2d5a983bd294b2f4b4270bb76105d5b
foxite <d.kok.2000@gmail.com>
2019-10-05 22:20:35 +0200

    added setting for opt-in, opt-out, and force

---
+++ settings.lua
@@ -0,0 +1,8 @@
+data:extend({{
+	type = "string-setting",
+	name = "squeakthrough-mod-compatibility",
+	order = "ad",
+	setting_type = "startup",
+	default_value = "opt-out",
+	allowed_values =  { "opt-out", "opt-in", "force" }
+}})
